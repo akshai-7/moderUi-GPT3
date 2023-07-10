b89d8384bc255521fd6dc87a78994fe178b1b8cc
Your Namekshai-7 <akshai2537@gmail.com>
2023-07-10 12:38:24 +0530

    brand commit

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,19 @@
-import "./brand.css"
+import "./brand.css";
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className="gpt3__brand section__padding">
+       <div className=""><img src={google} alt="google" /></div>
+       <div className=""><img src={slack} alt="slack" /></div>
+       <div className=""><img src={atlassian} alt="atlassian" /></div>
+       <div className=""><img src={dropbox} alt="dropbox" /></div>
+       <div className=""><img src={shopify} alt="shopify" /></div>
+    </div>
   )
 }
 
